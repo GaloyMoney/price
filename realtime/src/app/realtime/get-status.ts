2b9d9e539e53dfd25e8b4ba6daf5eabe358b9c89
Juan P Lopez <dolcalmi@gmail.com>
2023-03-31 10:31:30 -0500

    fix: update health validation with a threshold (#45)

--- realtime/src/app/realtime/get-status.ts
+++ realtime/src/app/realtime/get-status.ts
@@ -3,15 +3,23 @@ import { ServiceStatus } from "@domain/index"
 import { RealtimePriceRepository } from "@services/realtime-price"
 
 const realtimePriceRepository = RealtimePriceRepository()
+const USD = "USD" as CurrencyCode
+const THRESHOLD = 0.7
 
 export const getStatus = async (): Promise<ServiceStatus | ApplicationError> => {
+  const hasUsdActiveExchanges = await realtimePriceRepository.hasActiveExchanges(USD)
+  if (hasUsdActiveExchanges instanceof Error) return hasUsdActiveExchanges
+  if (!hasUsdActiveExchanges) return ServiceStatus.NOT_SERVING
+
+  let servingCurrencies = 0
   for (const currency of supportedCurrencies) {
     const hasActiveExchanges = await realtimePriceRepository.hasActiveExchanges(
       currency.code,
     )
     if (hasActiveExchanges instanceof Error) return hasActiveExchanges
-    if (!hasActiveExchanges) return ServiceStatus.NOT_SERVING
+    if (hasActiveExchanges) servingCurrencies++
   }
 
-  return ServiceStatus.SERVING
+  const servingRatio = servingCurrencies / supportedCurrencies.length
+  return servingRatio >= THRESHOLD ? ServiceStatus.SERVING : ServiceStatus.NOT_SERVING
 }
