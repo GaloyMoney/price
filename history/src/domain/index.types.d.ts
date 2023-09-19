d7e8aa4910b66fa9c742e0c37828e84eafc5a19c
Juan P Lopez <dolcalmi@gmail.com>
2023-09-19 15:05:44 -0500

    chore: bump deps + clean lint issues

--- history/src/domain/index.types.d.ts
+++ history/src/domain/index.types.d.ts
@@ -1,2 +1,2 @@
 type ServiceStatus =
-  typeof import("./index").ServiceStatus[keyof typeof import("./index").ServiceStatus]
+  (typeof import("./index").ServiceStatus)[keyof typeof import("./index").ServiceStatus]
