38bb170da2329d30d2afaf7b8e430ca68967175c
Kartik Shah <krtk.6160@gmail.com>
2024-04-09 00:05:26 +0530

    ci: use gh app for opening PRs

--- ci/tasks/open-charts-pr.sh
+++ ci/tasks/open-charts-pr.sh
@@ -35,6 +35,8 @@ Code diff contained in this image:
 https://github.com/GaloyMoney/price/compare/${old_ref}...${ref}
 EOF
 
+export GH_TOKEN="$(ghtoken generate -b "${GH_APP_PRIVATE_KEY}" -i "${GH_APP_ID}" | jq -r '.token')"
+
 gh pr close ${BOT_BRANCH} || true
 gh pr create \
   --title "chore(deps): bump-price-image-${ref}" \
